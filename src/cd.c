93832909d08bf294bbc9d693a4e55581bd63ebed
James PIRZENTHAL <jpirzent@e4r14p7.wethinkcode.co.za>
2018-09-18 12:35:19 +0200

    added stuff

--- src/cd.c
+++ src/cd.c
@@ -6,7 +6,7 @@
 /*   By: jpirzent <jpirzent@42.FR>                  +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2018/09/17 12:15:16 by jpirzent          #+#    #+#             */
-/*   Updated: 2018/09/18 10:17:35 by jpirzent         ###   ########.fr       */
+/*   Updated: 2018/09/18 12:35:06 by jpirzent         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,7 +18,8 @@ void		cd_noarg(void)
 	int		k;
 	char	*pwd;
 
-	i = find_var("HOME=");
+	if ((i = find_var("HOME=")) < 0)
+		ft_perror("HOME is unset");
 	pwd = get_pwd(env_cp[i]);
 	if (chdir(pwd) != 0)
 		ft_printf("\e[1;31mUnable to open that dir\e[0m\n");
@@ -29,22 +30,25 @@ void		cd_noarg(void)
 			new_pwd();
 			return ;
 		}
-		if ((k = find_var("OLDPWD=")) < 0)
+		else
 		{
-			new_opwd();
-			return ;
+			if ((k = find_var("OLDPWD=")) < 0)
+				ft_perror("OLDPWD is unset");
+			else
+			{
+				pwd = ft_strjoin("OLDPWD=", get_pwd(env_cp[i]));
+				change_line(pwd, k);
+				free(pwd);
+				if ((k = find_var("HOME=")) < 0)
+					ft_perror("HOME is unset");
+				else
+				{
+					pwd = ft_strjoin("PWD=", get_pwd(env_cp[k]));
+					change_line(pwd, i);
+					free(pwd);
+				}
+			}
 		}
-		pwd = ft_strjoin("OLDPWD=", get_pwd(env_cp[i]));
-		change_line(pwd, k);
-		free(pwd);
-		if ((k = find_var("HOME=")) < 0)
-		{
-			no_home();
-			return ;
-		}
-		pwd = ft_strjoin("PWD=", get_pwd(env_cp[k]));
-		change_line(pwd, i);
-		free(pwd);
 	}
 }
 
@@ -69,5 +73,22 @@ void		cd_warg(char *cwd)
 		pwd = ft_strjoin("PWD=", getcwd(path, sizeof(path)));
 		change_line(pwd, i);
 		free(pwd);
+	}
+}
+
+void	new_pwd(void)
+{
+	int		i;
+	int		k;
+	char	*pwd;
+
+	if ((k = find_var("OLDPWD=")) < 0)
+		ft_perror("OLDPWD is unset");
+	else
+	{
+		pwd = ft_strjoin("OLDPWD=", "");
+		change_line(pwd, k);
+		free(pwd);
+
 	}
 }
